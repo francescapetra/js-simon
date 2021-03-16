5bf3fd25cb135f91925fbc27a93559efacfd2be2
francescapetra <francescalaurapetra@gmail.com>
2021-03-16 14:14:15 +0100

    creazione cartelle

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// un alert espone 5 numeri casuali
+
+// parte un timer di 30 secondi
+
+// dopo 30 secondi abbimao 5 prompt dei nuemri visti
+
+// il software dice quali e quanti numeri sono stati indovinati
